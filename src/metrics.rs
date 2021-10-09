f543503278ad31852bae4959c6612e3b26b1b1a5
Devon Burriss <devon@chimplab.co>
2021-10-09 22:13:17 +0200

    Adds glob walking over dir to metrics command

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,7 +1,25 @@
 use crate::shared_types::MetricsConfig;
+extern crate globwalk;
+
+use std::fs;
+use std::path::Path;
 
 pub fn execute(config: MetricsConfig) {
     println!("Path: {}", config.repository_path);
     println!("Verbosity: {}", config.verbosity);
+    let base_dir = Path::new(&config.repository_path);
+
+    let walker = globwalk::GlobWalkerBuilder::from_patterns(base_dir, &["*.{cs,c,cpp,fs,js,java,rs,ts}", "!target/*"])
+        .build()
+        .unwrap()
+        .into_iter()
+        .filter_map(Result::ok);
+
+    let mut file_count = 0;
+    for file in walker {
+        println!("File: {}", file.file_name().to_string_lossy());
+        file_count += 1;
+    }
+    println!("File matches: {}", file_count);
 
 }
