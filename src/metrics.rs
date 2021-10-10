315fef6e774c0299d1254071d3e8d87e57c092e3
Devon Burriss <devon@chimplab.co>
2021-10-10 22:25:31 +0200

    perf improvements

--- src/metrics.rs
+++ src/metrics.rs
@@ -17,34 +17,37 @@ pub fn execute(config: MetricsConfig) {
         .into_iter()
         .filter_map(Result::ok);
 
-    let mut file_count = 0;
+    let mut files_scanned = 0;
+    let mut files_walked = 0;
 
     let timer = eggtimer::Timer::start();
 
     for de in walker {
         let dir_entry: DirEntry = de;
         let path = dir_entry.path();
-        let contentsOpt = fs::read(path);
-        if contentsOpt.is_ok() {
-            let contents = fs::read(path).unwrap();
-            let ext = path.extension().unwrap().to_str().unwrap();
-            let path_buf = path.to_path_buf();
-            let lang = rust_code_analysis::get_from_ext(path.extension().unwrap().to_str().unwrap());
-            match lang {
-                Some(LANG::Rust) => {
+        let lang = rust_code_analysis::get_from_ext(path.extension().unwrap().to_str().unwrap());
+        match lang {
+            Some(LANG::Rust) => {
+                let contentsOpt = fs::read(path);
+                if contentsOpt.is_ok() {
+                    let contents = fs::read(path).unwrap();
+                    let ext = path.extension().unwrap().to_str().unwrap();
+                    let path_buf = path.to_path_buf();
                     let parser = RustParser::new(contents, &path_buf, None);
                     let metrics = rust_code_analysis::metrics(&parser, &path_buf);
                     match metrics {
                         Some(m) => { println!("File: {} | Complexity: {}", path.display(), m.metrics.cyclomatic); },
                         None => ()
                     }
-                    file_count += 1;
-                },
-                _ => ()
-            }
+                    files_scanned += 1;
+                }
+            },
+            _ => ()
         }
+        files_walked += 1;
     }
     let time_taken_sec = timer.elapsed();
-    println!("File matches: {}", file_count);
+    println!("File scanned: {}", files_scanned);
+    println!("File matches: {}", files_walked);
     println!("Time taken: {}s", time_taken_sec);
 }
