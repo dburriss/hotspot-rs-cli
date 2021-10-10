36ff1a9034b7c0ce144747c507ffceca53bcba98
Devon Burriss <devon@chimplab.co>
2021-10-10 22:16:58 +0200

    Working metrics command

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Features:
 - list git contributors with metrics
 - list busfactor on files
 - recommend actions based on code metrics and git history
+
+## Developer notes
+
+- 2021/10/10: Was forced to drop down 4 versions to *rust-code-analysis* 0.0.19 as higher versions resulted in a conflict of 2 versions of *tree-sitter* being pulled in and getting a type mismatch on `get_language()`.
--- src/metrics.rs
+++ src/metrics.rs
@@ -3,13 +3,15 @@ extern crate globwalk;
 
 use std::fs;
 use std::path::Path;
+use rust_code_analysis::{LANG, ParserTrait, RustParser};
+use self::globwalk::DirEntry;
 
 pub fn execute(config: MetricsConfig) {
     println!("Path: {}", config.repository_path);
     println!("Verbosity: {}", config.verbosity);
     let base_dir = Path::new(&config.repository_path);
 
-    let walker = globwalk::GlobWalkerBuilder::from_patterns(base_dir, &["*.{cs,c,cpp,fs,js,java,rs,ts}", "!target/*"])
+    let walker = globwalk::GlobWalkerBuilder::from_patterns(base_dir, &["*.{cs,c,cpp,fs,go,js,java,py,rs,ts}", "!.*", "!**target/*"])
         .build()
         .unwrap()
         .into_iter()
@@ -19,12 +21,30 @@ pub fn execute(config: MetricsConfig) {
 
     let timer = eggtimer::Timer::start();
 
-    for file in walker {
-        //println!("File: {}", file.file_name().to_string_lossy());
-        file_count += 1;
+    for de in walker {
+        let dir_entry: DirEntry = de;
+        let path = dir_entry.path();
+        let contentsOpt = fs::read(path);
+        if contentsOpt.is_ok() {
+            let contents = fs::read(path).unwrap();
+            let ext = path.extension().unwrap().to_str().unwrap();
+            let path_buf = path.to_path_buf();
+            let lang = rust_code_analysis::get_from_ext(path.extension().unwrap().to_str().unwrap());
+            match lang {
+                Some(LANG::Rust) => {
+                    let parser = RustParser::new(contents, &path_buf, None);
+                    let metrics = rust_code_analysis::metrics(&parser, &path_buf);
+                    match metrics {
+                        Some(m) => { println!("File: {} | Complexity: {}", path.display(), m.metrics.cyclomatic); },
+                        None => ()
+                    }
+                    file_count += 1;
+                },
+                _ => ()
+            }
+        }
     }
     let time_taken_sec = timer.elapsed();
     println!("File matches: {}", file_count);
     println!("Time taken: {}s", time_taken_sec);
-
 }
