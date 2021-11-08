bca255b91b2d7b9039f1c5e79d7b70ea6fccec59
Devon Burriss <devon@chimplab.co>
2021-11-08 20:46:18 +0100

    Learning about git2 crate

--- src/contributors_cmd.rs
+++ src/contributors_cmd.rs
@@ -1,7 +1,28 @@
 use crate::shared_types::{ContributorsConfig};
+use std::path::{Path};
+use git2::Repository;
 
 pub fn execute(config: ContributorsConfig) {
-    let text = "bus factor";
+    let base_dir = Path::new(&config.repository_path);
+    // https://docs.rs/git2/0.13.22/git2/struct.Repository.html#method.reflog
+    let repo = match Repository::open(base_dir) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
 
-    println!("Run contributors on {}", config.repository_path);
+    let count = repo.references().unwrap().count();
+    println!("References: {}", count);
+    let head =  repo.head().unwrap();
+    let log = repo.reflog(head.name().unwrap()); // https://docs.rs/git2/0.13.22/git2/struct.Reflog.html 
+
+    let log_entry = log.iter().nth(0).unwrap().iter().nth(0).unwrap(); // https://docs.rs/git2/0.13.22/git2/struct.ReflogEntry.html
+    let commit_message = log_entry.message().unwrap();
+    let committer = log_entry.committer(); // https://docs.rs/git2/0.13.22/git2/struct.Signature.html
+    println!(
+        "Run contributors on {} at {} - message: {} from {}", 
+        &config.repository_path, 
+        head.name().unwrap(),
+        commit_message,
+        committer.name().unwrap()
+    );
 }
