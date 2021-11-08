8195e134e7fa7b688afb4764e1f72f7a9ec964be
Devon Burriss <devon@chimplab.co>
2021-11-08 22:07:14 +0100

    Prints ref commits

--- src/contributors_cmd.rs
+++ src/contributors_cmd.rs
@@ -1,5 +1,6 @@
 use crate::shared_types::{ContributorsConfig};
 use std::path::{Path};
+use std::collections::HashMap;
 use git2::Repository;
 
 pub fn execute(config: ContributorsConfig) {
@@ -9,20 +10,21 @@ pub fn execute(config: ContributorsConfig) {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
-
-    let count = repo.references().unwrap().count();
-    println!("References: {}", count);
+    let mut contributors : HashMap<String,u32> = HashMap::new();
+    // let count = repo.references().unwrap().count();
+    // println!("References: {}", count);
     let head =  repo.head().unwrap();
-    let log = repo.reflog(head.name().unwrap()); // https://docs.rs/git2/0.13.22/git2/struct.Reflog.html 
+    let logs = repo.reflog(head.name().unwrap()); // https://docs.rs/git2/0.13.22/git2/struct.Reflog.html 
 
-    let log_entry = log.iter().nth(0).unwrap().iter().nth(0).unwrap(); // https://docs.rs/git2/0.13.22/git2/struct.ReflogEntry.html
-    let commit_message = log_entry.message().unwrap();
-    let committer = log_entry.committer(); // https://docs.rs/git2/0.13.22/git2/struct.Signature.html
-    println!(
-        "Run contributors on {} at {} - message: {} from {}", 
-        &config.repository_path, 
-        head.name().unwrap(),
-        commit_message,
-        committer.name().unwrap()
-    );
+    logs.iter().for_each(|rlog| rlog.iter().for_each( |log_entry| { 
+        let committer = log_entry.committer();
+        let email = committer.email().unwrap().to_string();
+        *contributors.entry(email).or_insert(1) += 1;
+    }));
+
+    for k in contributors.keys() {
+        println!(
+            "{} with {} commits", k, contributors[k]
+        );
+    }
 }
